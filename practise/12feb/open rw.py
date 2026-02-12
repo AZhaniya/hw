6e5227e1635cd10d2b410e54f835caf51dfadf3f
Жания Аманкулова <zhaniya.amankulova@gmail.com>
2026-02-12 12:14:53 +0500

    practise 12.02

--- practise/12feb/open rw.py
+++ practise/12feb/open rw.py
@@ -1,43 +1,43 @@
 #1
-with open('data.txt' , 'w', encoding='utf-8') as f:
+with open('data.txt', 'w', encoding='utf-8') as f:
     f.write('Привет\n')
     f.write('что то пишу\n')
-with open('data.txt' , 'r', encoding='utf-8') as f:
+with open('data.txt', 'r', encoding='utf-8') as f:
     t=f.read()
     print(t)
 
 #2
-with open('file.txt' , 'w', encoding='utf-8') as f:
+with open('file.txt', 'w', encoding='utf-8') as f:
     for i in range(1,11):
         f.write(str(i)+'\n')
-with open('file.txt' , 'r', encoding='utf-8') as f:
+with open('file.txt', 'r', encoding='utf-8') as f:
     c=f.read()
     print(c)
 
 #3
-with open('names.txt' , 'w', encoding='utf-8') as f:
+with open('names.txt', 'w', encoding='utf-8') as f:
     f.write('Zhaniya\n')
     f.write('Magzhan\n')
     f.write('Arman\n')
 
-with open('names.txt' , 'r', encoding='utf-8') as f:
+with open('names.txt', 'r', encoding='utf-8') as f:
     n=f.read()
     print(n.upper())
 
 #1csv
 import csv
-with open('data.csv' , 'w', encoding='utf-8') as f:
+with open('data.csv', 'w', encoding='utf-8') as f:
     f.write('jknhibuy\n')
     f.write('вар\n')
-with open('data.csv' , 'r', encoding='utf-8') as f:
+with open('data.csv', 'r', encoding='utf-8') as f:
     s=f.read()
     print(s)
 
 #2
-with open('file.csv' , 'w', encoding='utf-8') as f:
+with open('file.csv', 'w', encoding='utf-8') as f:
     for i in range(1,11):
         f.write(str(i)+'\n')
-with open('file.csv' , 'r', encoding='utf-8') as f:
+with open('file.csv', 'r', encoding='utf-8') as f:
     c=f.read()
     print(c)
 
