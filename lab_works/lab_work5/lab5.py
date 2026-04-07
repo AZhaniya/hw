a100b959b8ad4879fe67948682a9a823c4a34c06
Жания Аманкулова <zhaniya.amankulova@gmail.com>
2026-04-07 18:24:33 +0500

    lab5 3 is made

---
+++ lab_works/lab_work5/lab5.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+@app.get("/")
+def root():
+    return {"status": "ok", "message": "FastAPI is running"}
+
+#1,2
+class User:
+    def __init__(self,id:int, name:str,email:str):
+        self._id=id
+        self._name=name.strip().title()
+        if '@' not in email:
+            raise ValueError
+        self._email = email.strip().lower()
+    def __str__(self):
+        return f"User(id={self._id}, name='{self._name}', email='{self._email}')"
+    def __del__(self):
+        return f"User {self._name} deleted"
+    @classmethod
+    def from_string(cls,data:str):
+        p=[x.strip() for x in data.split(',')]
+        if len(p)!=3:
+            raise ValueError
+        id=int(p[0])
+        name=p[1]
+        email=p[2]
+        return cls(id,name,email)
+
+@app.post('/task1')
+def task1():
+    u = User(1, " john doe ", "John@Example.COM")
+    return u
+
+@app.post('/task2')
+def task2():
+    u1= User.from_string("2, Alice Wonderland , alice@wonder.com")
+    return u1
+
+#3
+class Product:
+    def __init__(self,id:int,name:str,price:float,category:str):
+        self.id=id
+        self.name=name
+        self.price=price
+        self.category=category
+    def __str__(self):
+        return f"Product(id={self.id},name='{self.name}',price={self.price}, category='{self.category}')"
+    def  __eq__(self,other):
+        if not isinstance(other,Product):
+            return False
+        return self.id==other.id
+    def __hash__(self):
+        return hash(self.id)
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name':self.name,
+            'price':self.price,
+            'category':self.category
+        }
+@app.post('/task3')
+def task3():
+    pr=Product(1,'Laptop',1200.0,'electronic')
+    return pr
+
+#4
+class Inventory:
+    def __init__(self):
